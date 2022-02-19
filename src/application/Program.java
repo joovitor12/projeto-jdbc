684153a917f05ebf65600ea2222b063c03d175c8
Joao Vitor <outlast4321@gmail.com>
2022-02-19 01:39:32 -0300

    Seller entity created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "HQs");
 		System.out.println(obj);
+		Seller seller = new Seller(33, "John", "j@mail.com", new Date(), 7000.0, obj);
+		System.out.println(seller);
 
 	}
 
