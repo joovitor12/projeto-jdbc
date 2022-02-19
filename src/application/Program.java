dc186e1ed258066f80faeba6909cb6587b7d02b4
Joao Vitor <outlast4321@gmail.com>
2022-02-19 00:52:42 -0300

    Department entity created

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "HQs");
+		System.out.println(obj);
+
+	}
+
+}
